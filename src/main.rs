ed16bf32de0eab1987b866b948e7e65a68446a34
Nick Clark <nicholas.clark@gmail.com>
2017-01-05 22:37:30 -0700

    Added wizard-mode.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 extern crate rand;
+extern crate getopts;
+
 use std::io;
+use std::env;
 use std::cmp::Ordering;
 use rand::Rng;
+use getopts::Options;
 
 fn read_integer(prompt:&str)->u32 {
     let mut input = String::new();
@@ -17,13 +21,15 @@ fn read_integer(prompt:&str)->u32 {
     }
 }
 
-fn main() {
+fn guessing_game(wizard:bool) {
     println!("Guess the number!");
     
     let bound = read_integer("Set the upper bound.");
     let secret_number = rand::thread_rng().gen_range(1,bound + 1);
     
-    //println!("The secret number is: {}", secret_number);
+    if wizard {
+        println!("The secret number is: {}", secret_number);
+    }
     
     loop {
         let guess = read_integer("Please input your guess.");
@@ -38,4 +44,35 @@ fn main() {
             }
         }
     }
+}
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} [options]", program);
+    print!("{}", opts.usage(&brief));
+}
+
+fn main() {
+    let args:Vec<String> = env::args().collect();
+    let program = args[0].clone();
+
+    let mut opts = Options::new();
+    let mut wizard = false;
+
+    opts.optflag("h", "help", "Print this help menu");
+    opts.optflag("w", "wizard", "Print the answer, you cheater");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => {m}
+        Err(f) => {panic!(f.to_string())}
+    };
+
+    if matches.opt_present("h") {
+        print_usage(&program, opts);
+        return;
+    }
+    
+    if matches.opt_present("w") {
+        wizard = true;
+    }
+    
+    guessing_game(wizard);
 }
